13220adede63eb60b1feae28428902272ef7847a
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-03 18:36:38 +0300

    количество четных чисел в массиве

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int size =4;
+int [ ] array = new int [size];
+int minvalue = 100;
+int maxvalue = 1000;
+int count = 0;
+for (int i = 0; i < array.Length;i++)
+{
+    array[i] = new Random().Next(minvalue,maxvalue);
+    
+
+if(array[i] %2 == 0)
+
+count ++;}
+Console.Write("[");
+Console.Write(string.Join("," , array));
+Console.Write("]=>");
+
+ Console.WriteLine( count);  
